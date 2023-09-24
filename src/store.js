9d0e9a9fe5bb1e3fad21000d66d699dd95060eae
Vishal Sindham <37668661+VishalSindham@users.noreply.github.com>
2023-09-24 12:35:27 +0530

    Part four completed redux dev tools added

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
-import { createStore, compose } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducer";
 import { 
     sayHiOnDispatch, 
     includeMeaningOfLife 
 } from "./exampleAddons/enhancers";
-
+// import { print1, print2,print3 } from "./exampleAddons/middleware"
 /* 
 * createStore accepts state as the second argument which
 * we can fetch from server or localstorage and 
@@ -20,12 +20,36 @@ if(persistedTodosString){
     }
 }
 
-const composedEnhancer = compose(sayHiOnDispatch, includeMeaningOfLife)
+function exampleMiddleware(storeAPI){
+    return function wrapDispatch(next){
+        return function handleAction(action){
+            console.log("inside a custom middleware function")
+            console.log('dispatching', action)
+            let result = next(action)
+            console.log('next state ', storeAPI.getState())
+            return result
+        }
+    }
+}
+
+const delayedMessageMiddleware = storeAPI => next => action => {
+    if(action.type === 'todos/todoAdded'){
+        setTimeout(() => {
+            console.log("Added a new todo : ", action.payload)
+        }, 1000)
+    }
+    return next(action)
+}
+
+const middlewareEnhancer = applyMiddleware(exampleMiddleware,delayedMessageMiddleware);
+const composeWithDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composedEnhancer = composeWithDevTools(sayHiOnDispatch, includeMeaningOfLife, middlewareEnhancer)
 
 const store = createStore(
     rootReducer, 
     preloadState,
     composedEnhancer,
+    // middlewareEnhancer
 )
 
 // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
