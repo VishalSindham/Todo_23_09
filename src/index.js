5b80848769eaa3411a66891e8da5dbbd09a1ac54
Vishal Sindham <vszb101@gmail.com>
2023-09-24 15:47:59 +0530

    Part 5 completed from the desktop

--- src/index.js
+++ src/index.js
@@ -1,59 +1,17 @@
-/*
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
 import './index.css'
 import App from './App'
-
-import './api/server'
+// import './api/server'
+import store from './store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
-*/
-
-import store from './store';
-
-// Log the initial state
-console.log('Initial State:', store.getState())
-// {todos: [...], filters:{status,colors}}
-
-/*
-* Every time the state changes, log it
-* Note that subscribe() returns a function for unregistering the listener
-*/
-
-const unsubscribe = store.subscribe(() => 
-        console.log("State after dispatch ", store.getState())
-)
-
-// Now, dispatch some actions 
-console.log('Dispatching action')
-store.dispatch({type:"todos/todoAdded", payload:"Learn about actions"})
-console.log('Dispatching complete')
-store.dispatch({type:"todos/todoAdded", payload:"Learn about reducers"})
-store.dispatch({type:"todos/todoAdded", payload:"Learn about stores"})
-
-store.dispatch({type:"todos/todoToggled", payload:0})
-store.dispatch({type:"todos/todoToggled", payload:1})
-
-store.dispatch({type:"filters/statusFilterChanged", payload:"Active"})
-
-store.dispatch({
-  type: "filters/colorFilterChanged",
-  payload: { color: "red", changeType: "added" }
-})
-
-
-// stop listening to the state updates 
-unsubscribe()
-
-// Dispatch one more action to see what happens 
-
-console.log('Dispatching action after unsubscribing')
-store.dispatch({type:"todos/todoAdded", payload:"Dispatch after unsubscribe"})
-console.log('Dispatching complete after unsubscribing')
-
-// All previous render code has been commented.
